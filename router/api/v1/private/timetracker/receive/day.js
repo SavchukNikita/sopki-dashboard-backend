2b38c07077e8d6b83d28400af5539d2473b6e51d
Nikita <nikita.savchuk.2000@gmail.com>
2021-06-06 00:14:06 +1000

    get day time stat

--- router/api/v1/private/timetracker/receive/day.js
+++ router/api/v1/private/timetracker/receive/day.js
@@ -14,12 +14,29 @@ const day = async (req, res) => {
     const result = [];
 
     for (let i = 0; i < docs.length; i += 1) {
+      let today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0');
+      const yyyy = today.getFullYear();
+      today = `${yyyy}.${mm}.${dd}`;
+
       // eslint-disable-next-line no-await-in-loop
-      const wakatimeRes = await wakatime.get(`/api/v1/users/current/orgs/fabdc280-4d17-4f63-97af-39cbd64cf602/dashboards/6f4a75d8-0fae-42be-816d-d564a5981f4c/members/${docs[i].wakatimeId}/durations?date=2021.06.03`);
-      console.log(wakatimeRes);
-      console.log(docs[i]);
-      if (wakatimeRes.data.data) {
-        result.push(wakatimeRes.data.data);
+      const wakatimeRes = await wakatime.get(`/api/v1/users/current/orgs/fabdc280-4d17-4f63-97af-39cbd64cf602/dashboards/6f4a75d8-0fae-42be-816d-d564a5981f4c/members/${docs[i].wakatimeId}/durations?date=${today}`);
+      if (wakatimeRes.data && wakatimeRes.data.data) {
+        let duration = 0;
+
+        for (let j = 0; j < wakatimeRes.data.data.length; j += 1) {
+          duration += wakatimeRes.data.data[j].duration;
+        }
+
+        const userObj = {
+          firstname: docs[i].firstname,
+          lastname: docs[i].lastname,
+          _id: docs[i]._id,
+          duration,
+        };
+
+        result.push(userObj);
       }
     }
 
