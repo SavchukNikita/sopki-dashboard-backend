fb62710c61b734573c5a5b356d6857bcdc6d32f0
Nikita <nikita.savchuk.2000@gmail.com>
2021-06-13 19:19:40 +1000

    cors

--- router/api/api.js
+++ router/api/api.js
@@ -3,10 +3,6 @@ import v1 from './v1/v1.js';
 
 const appRouter = router();
 
-appRouter.use('/v1', (req, res) => {
-  // res.header('Access-Control-Allow-Credentials', 'true');
-
-  v1(req, res);
-});
+appRouter.use('/v1', v1);
 
 export default appRouter;
