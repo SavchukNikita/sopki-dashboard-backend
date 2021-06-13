03057681bd1536da35bd885a9ffda6249503d728
Nikita <nikita.savchuk.2000@gmail.com>
2021-06-13 18:25:34 +1000

    return auth

--- router/api/api.js
+++ router/api/api.js
@@ -3,6 +3,12 @@ import v1 from './v1/v1.js';
 
 const appRouter = router();
 
-appRouter.use('/v1', v1);
+appRouter.use('/v1', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+
+  v1(req, res);
+});
 
 export default appRouter;
