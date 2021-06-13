673db9de454192843449d49cc923ce56f952d53b
Nikita <nikita.savchuk.2000@gmail.com>
2021-06-13 18:58:23 +1000

    cors fix

--- router/api/api.js
+++ router/api/api.js
@@ -4,10 +4,7 @@ import v1 from './v1/v1.js';
 const appRouter = router();
 
 appRouter.use('/v1', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
 
   v1(req, res);
 });
